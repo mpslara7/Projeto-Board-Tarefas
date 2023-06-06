a7ea1ec9cc9edae902371355130c13a1d2ff8b52
mplara7 <mpslara11@gmail.com>
2023-06-06 12:28:13 -0300

    creating-function-delete-comments

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -13,6 +13,7 @@ import {
   getDoc,
   addDoc,
   getDocs,
+  deleteDoc,
 } from 'firebase/firestore';
 import { TextArea } from '@/components/textArea';
 import { FaTrash } from 'react-icons/fa';
@@ -72,6 +73,15 @@ export default function Task({ item, allComments }: TaskProps) {
     }
   }
 
+  async function handleDeleteComment(id: string) {
+    try {
+      const docRef = doc(db, 'comments', id);
+      await deleteDoc(docRef);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -113,7 +123,10 @@ export default function Task({ item, allComments }: TaskProps) {
             <div className={styles.headComment}>
               <label className={styles.commentsLabel}>{item.name}</label>
               {item.user === session?.user?.email && (
-                <button className={styles.buttonTrash}>
+                <button
+                  className={styles.buttonTrash}
+                  onClick={() => handleDeleteComment(item.id)}
+                >
                   <FaTrash size={18} color="#EA3140" />
                 </button>
               )}
